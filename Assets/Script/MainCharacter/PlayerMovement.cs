59de28ee6e60ad50019b6bb93d14df8a473cb678
xiaojunfeng <xiaojunfeng@sjtu.edu.cn>
2025-03-29 17:38:59 +0800

    feat: initiate UI for balance point

--- Assets/Script/MainCharacter/PlayerMovement.cs
+++ Assets/Script/MainCharacter/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,14 +10,47 @@ public class PlayerMovement : MonoBehaviour
     private float movementSpeed = 10;
     Animator playerAnimator;
     PlayerAnimator playerAniamationScript;
+    private Canvas balanceIndicatorCanvas;
+    private Image balanceIndicatorLeft;
+    private Image balanceIndicatorLeftMid;
+    private Image balanceIndicatorMid;
+    private Image balanceIndicatorRightMid;
+
+    private Image balanceIndicatorRight;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         playerAnimator = GetComponentInChildren<Animator>();
         playerAniamationScript = GetComponentInChildren<PlayerAnimator>();
-    }
+        balanceIndicatorCanvas = GetComponentInChildren<Canvas>();
+        balanceIndicatorLeft = balanceIndicatorCanvas.transform.Find("left").GetComponent<Image>();
+        balanceIndicatorLeftMid = balanceIndicatorCanvas.transform.Find("leftmid").GetComponent<Image>();
 
+        balanceIndicatorMid = balanceIndicatorCanvas.transform.Find("mid").GetComponent<Image>();
+
+        balanceIndicatorRightMid = balanceIndicatorCanvas.transform.Find("rightmid").GetComponent<Image>();
+
+        balanceIndicatorRight = balanceIndicatorCanvas.transform.Find("right").GetComponent<Image>();
+
+        initBalanceIndicator();
+
+    }
+    void initBalanceIndicator()
+    {
+        RectTransform rectTransform1 = balanceIndicatorLeft.GetComponent<RectTransform>();
+        rectTransform1.anchoredPosition = new Vector2(0, -500);
+        RectTransform rectTransform2 = balanceIndicatorLeftMid.GetComponent<RectTransform>();
+        rectTransform2.anchoredPosition = new Vector2(150, -500);
+        RectTransform rectTransform3 = balanceIndicatorMid.GetComponent<RectTransform>();
+        rectTransform3.anchoredPosition = new Vector2(300, -500);
+        RectTransform rectTransform4 = balanceIndicatorRightMid.GetComponent<RectTransform>();
+        rectTransform4.anchoredPosition = new Vector2(450, -500);
+        RectTransform rectTransform5 = balanceIndicatorRight.GetComponent<RectTransform>();
+        rectTransform5.anchoredPosition = new Vector2(600, -500);
+        balanceIndicatorMid.color = Color.red;
+    }
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
@@ -26,7 +60,7 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = Vector3.zero;
         }
-        else if (horizontal != 0f )
+        else if (horizontal != 0f)
         {
 
             rb.velocity = new Vector3(horizontal, 0, 0) * movementSpeed;
